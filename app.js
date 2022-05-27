a53b2515ec308d46515664b6d90221b3caf7164e
Jitin <jitin.a27.jd@gmail.com>
2022-05-27 13:43:32 +0530

    one

---
+++ app.js
@@ -0,0 +1,19 @@
+const express=require("express");
+const bodyparser=require("body-parser");
+const app=express();
+app.set('view engine','ejs');
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.get("/driver",function(req,res){
+  res.send("driver login");
+});
+app.get("/client",function(req,res){
+  res.send("client login");
+});
+app.get("/",function(req,res){
+  res.render("index");
+});
+
+app.listen(3000,function(){
+  console.log("server up an drunning on port 3000");
+});
